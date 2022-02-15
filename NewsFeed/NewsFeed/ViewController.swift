a4ab5d39b1f02f616a56409e4f09664bf5c6c496
EugeneStarshov <yeenzo@yandex.ru>
2022-02-15 16:24:27 +0300

    first commit

---
+++ NewsFeed/NewsFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsFeed
+//
+//  Created by Евгений Старшов on 15.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
